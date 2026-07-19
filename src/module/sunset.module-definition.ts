c859fcd09db2547fc7a59287afc920b733a05585
LeDevNovice <gregory.saison@proton.me>
2026-07-19 15:47:33 +0200

    feat(module): add module token and define set extra

--- src/module/sunset.module-definition.ts
+++ src/module/sunset.module-definition.ts
@@ -1,6 +1,12 @@
 import { ConfigurableModuleBuilder } from '@nestjs/common';
 
-export const {} = new ConfigurableModuleBuilder()
+export const {
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN: SUNSET_OPTIONS_TOKEN, // to avoid name collision with other modules
+} = new ConfigurableModuleBuilder({ moduleName: 'Sunset' })
   .setClassMethodName('forRoot')
-  .setExtras({ isGlobal: false })
+  .setExtras({ isGlobal: false }, (definition, extras) => ({
+    ...definition,
+    global: extras.isGlobal,
+  }))
   .build();
