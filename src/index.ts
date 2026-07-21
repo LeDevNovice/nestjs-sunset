c85b2abdf3f176c0efaae1a14cce68ad53fd026e
LeDevNovice <gregory.saison@proton.me>
2026-07-21 21:26:07 +0200

    fix(import): add missing imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+export { Deprecated } from './decorators/deprecated.decorator';
+
 export { SunsetModule } from './module/sunset.module';
+
+export type { DeprecatedOptions } from './types/deprecated-options.type';
+export type { DeprecationEvent } from './types/deprecation-event.type';
 export type { SunsetModuleOptions } from './module/sunset-module.options';
